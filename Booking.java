9ef4ccfe637f57b24dbe4a9d9c678afc1812fd8c
klu2300031386 <163740076+klu2300031386@users.noreply.github.com>
2025-05-11 09:24:04 +0530

    Add files via upload

---
+++ Booking.java
@@ -0,0 +1,73 @@
+package com.klu.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String eventName;
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getVenue() {
+		return venue;
+	}
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getSeatCategory() {
+		return seatCategory;
+	}
+	public void setSeatCategory(String seatCategory) {
+		this.seatCategory = seatCategory;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	private String venue;
+    private LocalDateTime dateTime;
+    private String city;
+    private String seatCategory;
+    private double price;
+    private String username;
+
+    // Getters and Setters ...
+}
