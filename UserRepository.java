815a676fb9733568225d0a95fdb3c3de623e81c3
klu2300031386 <163740076+klu2300031386@users.noreply.github.com>
2025-05-11 09:25:06 +0530

    Add files via upload

---
+++ UserRepository.java
@@ -0,0 +1,12 @@
+package com.klu.repository;
+
+import com.klu.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
